77951f34313f9727ea5abe1b0bb3c92044627671
technocrat <public@careaga.net>
2022-07-27 00:11:49 -0700

    Revised functions to take only a single row of data and designed passer and receiver logic

--- R/etl.R
+++ R/etl.R
@@ -9,52 +9,78 @@ source(here::here("R/prepare.R"))
 
 # DATA
 
-tic()
-o <- fread(here("obj/splits/1000.csv"))
-toc()
-
-# apply the scaling function, scale_ndvi
-
-# 500x780 data.table of scaled data returned in 0.5 sec
-# about 30 minutes to handle the whole thing
-# might be parallel, but need record id, because currently
-# the row id is the implicit id
-# scale_ndvi(d)  |> str()
-# tic() result r is a data.table/data.frame
-r <- scale_ndvi(o)
-# toc()
-
-# we can write back quite quickly with
-fwrite(r, file = "scaled.csv", append = TRUE)
-
-# if we do this 4,000 times, probably not too long
-
-# assume we are now working with reading back in
-# as the scaled dataset, now called s, in chunks of 2000
-
-s <- fread(here::here("scaled.csv"))
-
-# chunks <- dir(here("obj/chunks"), pattern = "\\.csv$", full.names = TRUE)
+chunks <- dir(here("obj/chunks"), pattern = "\\.csv$", full.names = TRUE)
 leftover <- fread(here("chunks/chunk_1047.csv")) # only 387 lines
 
-pix_list <- vector(mode = "list",length = chunk_size)
-#m <- TBD
-# save out chunk_1047.csv for separate handling
-# the order of pix_length will reflect the order in
-# which chunks are read; if necessary receiver object
-# can be pix sorted
+# ACCUMULATION OBJECTS
 
-for(i in seq_along(chunks - 1)){
-  the_chunk = fread(chunks[i])
-  pix_list[i] = the_chunk[i]
-  the_na = count_na(the_chunk)
-  the_ice = find_frozen(the_chunk)
-  the_chunk = interpolate(the_chunk)
-  the_chunk = make_splines(the_chunk)
-  the_chunk = make_smooth(the_chunk)
-  the_chunk = make_sss(the_chunk)
+# brings empty data.table to take output of for loops below
+
+receiver <- source(here("R/receiver.R"))
+passer <- receiver
+# exclude last chunk, which has an off number of rows
+
+for(chunk in seq_along(chunks-1)) {
+  for(i in seq_along(chunk)) {
+    # accumulate results in a vector
+    v = vector(length = 12)
+    v[1] = chunk[,1] # pix value for this chunk
+    the_chunk = scale_ndvi(chunk)
+    # discard pix
+    the_chunk = the_chunk[,-1]
+    v[11] = length(which(is.na(the_chunk)))
+    v[12] = length(
+      which(the_chunk - 
+              floor(the_chunk/10)*10 + 1 == 4 | 
+              the_chunk - 
+              floor(the_chunk/10)*10 + 1 == 6))
+    the_chunk = interpolate(the_chunk)
+    the_chunk = make_splines(the_chunk)
+    v[2:8] = make_smooth(the_chunk)
+    v[9] = make_sss(the_chunk)
+    v[10] = L.JDAY
+    # accumulate into data.table
+    passer = passer
+  }
 }
 
+# last chunk
+for(chunk in seq_along(leftover)) {
+  for(i in seq_along(chunk)) {
+    
+    the_chunk = scale_ndvi(chunk)
+    pix_list = vector(mode = "list",length = chunk_size)
+    pix_list[i] = the_chunk[i,1]
+    the_chunk = the_chunk[,-1]
+    the_na = length(which(is.na(the_chunk)))
+    the_ice = length(
+      which(the_chunk - 
+              floor(the_chunk/10)*10 + 1 == 4 | 
+              the_chunk - 
+              floor(the_chunk/10)*10 + 1 == 6))
+    the_chunk = interpolate(the_chunk)
+    the_chunk = make_splines(the_chunk)
+    the_smooth = make_smooth(the_chunk)
+    make_sss(the_chunk)
+  }
+}
+passer <- data.table(
+  # pix is the record id added corresponding to original row order
+  pix = 0,
+  # variables derived from the smoothed splines
+  lq = 0,0,
+  uq = 0.0,
+  mean.evi = 0.0,
+  sd.evi = 0.0,
+  sum.evi = 0.0,
+  amplitude = 0.0,
+  devi.ss = 0.0,
+  devi.sss = 0.0,
+  # a constant from cons.R
+  L.JDAY = 0,
+  # variables derived from from scaled data
+  NA.length = 0,
+  ICE.length = 0
+)
 
-
-
+for(i in seq_along(v)) passer[1,i] = v[i]
--- R/func.R
+++ R/func.R
@@ -1,50 +1,16 @@
 # func.R
-# functions used in scripts under this directory
+# functions used in scripts in this directory
 # author: Richard Careaga
 # Date: 2022-07-20
 
-# how many NA data points on scaled data 
-
-count_na <- function(x,y = chunk_size) {
-  v <- vector(length = chunk_size)
-  for(i in 1:y) v[i] = length(which(is.na(x)))
-  return(v)
-}
-
-
-# takes a row vector of the scaled data and returns a scalar
-
-find_frozen <- function(x,y = chunk_size){
-  # how many possible ICE/SNOW data points
-  v <- vector()
-  # remove pix column
-  x = x[,-1]
-  for(i in 1:y) {
-    #q = x[i,] - floor(unlist(x[i,])/10)*10 + 1
-    v[i] = length(which((unlist(x[i,]) - 
-                           floor(unlist(x[i,])/10)*10 + 1) == 4 | 
-                        (unlist(x[i,]) - 
-                           floor(unlist(x[i,])/10)*10 + 1) == 6))  
-  }
-  return(v)
-}
-
 # interpolate missing values of scaled data
+# takes a single row of the scaled data
  
-interpolate <- function(x,y=chunk_size) {
-  m = matrix(0,chunk_size,col_width)
-  for(i in 1:chunk_size) {
-    m[i,] = approx(JDAY.x,unlist(x[i,-1]), xout = JDAY.x,  rule = 2)$y
-  }
-  return(m)
-}
+interpolate <- function(x) approx(JDAY.x,x, xout = JDAY.x, rule = 2)$y
 
-# takes scaled data
+# takes a single row of the scaled data
 
 make_splines <- function(x) {
-  # set aside record id
-  pix = x[,1]
-  x   = x[,-c(1)]
   # get a filtered running mean:
   splines = filter(x,
         filter = rep(1 / 3,3),
@@ -52,68 +18,52 @@ make_splines <- function(x) {
         sides = 2, # 2022-07-21 00:20 -07:00 "side" in the original
         circular = TRUE
     )
-  # reunite with pix
-  splines = cbind(pix,splines)
   return(splines)
 } 
 
+# takes a single row of the scaled data
 
 make_smooth <- function(x){
-  # adjust col width to reflect removal of first and last values
-  #ss = matrix(0,chunk_size,col_width - 2) 
-  for(i in 1:chunk_size)  {
-    # TODO fix hardwired 781
-    ss = smooth.spline(JDAY.x[-c(2,col_width - 1)],
-                       x[i,-c(1:2,781)])$y
-    # TODO: need to capture pix, not i
-    pix = i
-    lq = quantile(ss, 0.1)
-    uq = quantile(ss, 0.9)
-    amplitude = uq - lq
-    # TODO: write to receiver object
-    # the mean evi of the time series
-    mean.evi = mean(ss)
-    # the standard deviation of the time series
-    sd.evi = sd(ss)
-    # sum evi
-    sum.evi = sum(ss)
-    # deviation between less smooth spline and data
-    devi.ss <- mean(unlist(abs(s[i,-c(1:2,781)]-ss))) 
-    results = c(pix,lq,uq,amplitude,mean.evi,sd.evi,sum.evi,devi.ss)
-    names(results) <- c("pix","lq","uq","amplitude","mean.evi","sd.evi","sum.evi","devi.ss")
-    return(results)
-  }
+  ss = smooth.spline(JDAY.x[-c(2,col_width - 1)],
+                     x[-c(1,col_width - 1)])$y
+  lq = quantile(ss, 0.1)
+  uq = quantile(ss, 0.9)
+  amplitude = uq - lq
+  # the mean evi of the time series
+  mean.evi = mean(ss)
+  # the standard deviation of the time series
+  sd.evi = sd(ss)
+  # sum evi
+  sum.evi = sum(ss)
+  # deviation between less smooth spline and data
+  devi.ss <- mean(unlist(abs(x[-c(1,col_width - 1)]-ss))) 
+  results = c(lq,uq,amplitude,mean.evi,sd.evi,sum.evi,devi.ss)
+  names(results) <- c("lq","uq","amplitude","mean.evi","sd.evi","sum.evi","devi.ss")
+  return(results)
 }
 
-# takes the scaled data
+# takes a single row of the scaled data
 
 make_sss <- function(x){
-  # adjust col width to reflect removal of first and last values
-  #sss <- matrix(0,chunk_size,col_width - 2) 
-  # return object
-  v = vector()
-  for(i in 1:chunk_size)  {
-    # TODO fix hardwired 781
-    sss_both = smooth.spline(JDAY.x[-c(2,col_width - 1)],
-                         x[i,-c(1:2,781)],
-                         spar = 0.3)
-    sss.x = sss_both$x
-    sss.y = sss_both$y
-    d1 = predict(sss_both, sss.x, deriv = 1)$y
-    d1 = (d1 - mean(d1)) / sd(d1)
-    d2 = predict(sss_both, sss.x, deriv = 2)$y
-    d2 = (d2 - mean(d2)) / sd(d2)
-    # deviation between smoother spline and data
-    devi.sss = mean(unlist(abs(x[i,-c(1:2,781)] - sss.y)))  
-    return(list(d1,d2,devi.sss))
-    }
+  sss_both = smooth.spline(JDAY.x[-c(2,col_width - 1)],
+                           x[-c(2,col_width -1)],
+                           spar = 0.3)
+  sss.x = sss_both$x
+  sss.y = sss_both$y
+  d0 = predict(sss_both, sss.x, deriv = 1)$y
+  d1 = (d0 - mean(d0)) / sd(d0)
+  d0 = predict(sss_both, sss.x, deriv = 2)$y
+  d2 = (d0 - mean(d0)) / sd(d0)
+  # deviation between smoother spline and data
+  devi.sss = mean(abs(x[-c(2,col_width -1)] - sss.y))
+  return(devi.sss)
 }
 
 # save the current chunk's pix column
 
 save_pix <- function(x) x[,1]
 
-# scale source data
+# scale source data (not row-by-row)
 
 scale_ndvi <- function(x) {
   # save out record id, pix
@@ -128,4 +78,3 @@ scale_ndvi <- function(x) {
   e = cbind(pix,e)
   return(e)
 }
-

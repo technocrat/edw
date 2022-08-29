adf275b4025d9236f2a726a6ad7445aa7b433ded
technocrat <public@careaga.net>
2022-08-29 15:27:09 -0700

    Almost to windowing

--- R/func.R
+++ R/func.R
@@ -1,7 +1,9 @@
 # func.R
 # functions used in scripts in this directory
 # author: Richard Careaga
-# Date: 2022-v07-20
+# Date: 2022-08-29
+
+adjust_trough.day <- function(x) ifelse(x > 188, x, x + 365)
 
 circ.mean <- function(x) {
   x = rad(x * (360 / 365))
@@ -13,22 +15,19 @@ circ.mean <- function(x) {
   return(circmean)
 }
 
-rad <- function(x) (x * pi)/180
-
-deg <- function(x) (x * 180)/pi
-
 find_r_value <- function(x) x - floor(x/10) * 10 + 1
 
 find_mean_abs_ss <-    function(x) mean(abs(x - ss))
-find_mean_abs_sss_y <- function(x) mean(abs(x - smooth.spline(JDAY.x,x,spar = 0.3)$y))
+find_mean_abs_sss_y <- function(x) mean(abs(x - get_sss(x)$y))
 
 get_sss <- function(x) smooth.spline(JDAY.x, x, spar=0.3)
+get_sss_y <- function(x) smooth.spline(JDAY.x, x, spar=0.3)$y
 
 get_quantiles <- function(x) quantile(x,probs = c(0.1,0.9),na.rm = TRUE)
 
 # interpolate missing values of scaled data
 
-interpolate <- function(x) t(approx(JDAY.x,x, xout = JDAY.x, rule = 2)$y)
+interpolate <- function(x) approx(JDAY.x,x, xout = JDAY.x, rule = 2)$y
 
 make_splines <- function(x) {
   # get a filtered running mean: 2022-07-30 14:17 -07:00
@@ -46,31 +45,13 @@ make_splines <- function(x) {
   return(t(splines))
 } 
 
-make_smooth <- function(x){
-  #ss is the smoothed spline on filtered data
-  ss = x
-  lq = quantile(ss, 0.1)
-  uq = quantile(ss, 0.9)
-  amplitude = uq - lq
-  # the mean evi of the time series
-  mean.evi = mean(ss)
-  # the standard deviation of the time series
-  sd.evi = sd(ss)
-  # sum evi
-  sum.evi = sum(ss)
-  # deviation between less smooth spline and data
-  devi.ss <- mean(unlist(abs(x[-c(1,col_width - 1)]-ss))) 
-  results = c(lq,uq,amplitude,mean.evi,sd.evi,sum.evi,devi.ss)
-  names(results) <- c("lq","uq","amplitude","mean.evi","sd.evi","sum.evi","devi.ss")
-  return(results)
-}
-
 make_sss <- function(x){
-  sss.x = sss_both$x
-  sss.y = sss_both$y
-  d0 = predict(sss_both, sss.x, deriv = 1)$y
-  d1 = (d0 - mean(d0)) / sd(d0)
-  d0 = predict(sss_both, sss.x, deriv = 2)$y
+  sss = get_sss(x)
+  d1 = predict(sss, sss$x, deriv = 1)$y
+  d1 = (d1 - mean(d1)) / sd(d1)
+  d2 = predict(sss, sss$x, deriv = 2)$y
+  d2 = (d2 - mean(d2))/sd(d2)
+  return(list(d1,d2))
 }
 
 scale_ndvi <- function(x) {
--- R/peak_trough.R
+++ R/peak_trough.R
@@ -19,22 +19,23 @@ d <- fread(here("obj/chunks/chunk_1.csv"))
 # remove 'pix` identifier column
 d <- d[,-1]
 
+
 # use base matrix, as data.table::as.matrix transposes
-d <- base::as.matrix(d,)
+#d <- base::as.matrix(d,)
 
 # MAIN
 
 ## Scale raw ndvi data by factor of 1e-4
 
 # scale_ndvi converts to 780 2000, so transpose
-d <- t(apply(d,1,scale_ndvi))
+
+d <- d[, lapply(.SD, scale_ndvi)]
 
 # interpolate returns the equivalent of approx.e
 
 ## interpolate missing values
 
-d <- t(apply(d,1,interpolate))
-
+d <- apply(d,1,interpolate)
 # save out a copy for use in calculating sss
 # d will be further pre-processed and used to
 # calculate ss
@@ -55,53 +56,66 @@ d[,1] <- begin
 d[,col_width] <- finish
 
 ## smooth spline on filtered data
-ss <- apply(d,1,smooth_spline)
+ss <- apply(t(d),1,smooth_spline)
+
+# convert d back to data.table to add variables
+d <- data.table(d)
+# TODO:  Verify use of quantiles rather than max/min
+
+d[,lq := quantile(ss, 0.1)]
+d[,uq := quantile(ss, 0.9)]
+d[,amplitude := uq - lq]
+d[,mean.evi := mean(ss)]
+d[,sd.evi := sd(ss)]
+d[,sum.evi := sum(ss)]
 
 # deviation between less smooth spline and data
 devi.ss <- apply(approx.e,1,find_mean_abs_ss)
 
 # deviation between smoother spline and data
-devi.sss <- apply(approx.e,1,find_mean_abs_sss_y)
+devi.sss <- apply(t(approx.e),1,find_mean_abs_sss_y)
 
-# TODO:  Verify use of quantiles rather than max/min
+sss_y <- apply(t(approx.e),1,get_sss_y)
 
 # convert to data.table because dates and doubles in the 
 # same matrix will convert to typeof character
+# TODO:  Verify use of quantiles rather than max/min
+
 sss_y <- data.table(sss_y)
 sss_y <- cbind(Date = ymd(the_dates),sss_y)
 sss_y[, jday:=yday(Date)]
 sss_y[, Year:=year(Date)]
 sss_y[,year_min := min(jday),Year]
 the_min_rows <- which(sss_y$jday == sss_y$year_min)
-sss_y[the_min_rows,] 
 sss_y[the_min_rows,mean(jday)]
 sss_y[,year_max := max(jday),Year]
 the_max_rows <- which(sss_y$jday == sss_y$year_max)
-sss_y[the_max_rows,] 
 sss_y[the_max_rows,mean(jday)]
 
 ## calculate peak.day, trough.day
 
+# TODO:  Why is there a single time series for all pixels?
+
 peak.day <- unique(sss_y[,year_max, by = "YEAR"])
 trough.day <- unique(sss_y[,year_min, by = "YEAR"])
-peak.day <- day.numbers[by(sss_y, YEAR, which.max)]
-peak.day <- round(circ.mean(peak.day[-1]))
-trough.day <- round(circ.mean(trough.day[-1]))
+
+peak.day <- round(apply(peak.day[,-1],1,circ.mean))
+trough.day <- round(apply(trough.day[,-1],1,circ.mean))
 
 # smoother spline and derivatives
 
-# TODO:  how to extract what sss$x is supposed to be
-sss <- apply(t(ss),1,get_sss)
-d1 <- predict(sss, sss$x, deriv = 1)$y
-d1 <- (d1 - mean(d1)) / sd(d1)
-d2 <- predict(sss, sss$x, deriv = 2)$y
-d2 <- (d2 - mean(d2)) / sd(d2)
+# ds contains lists for d1 and d2, each of which is a list of lists
+ds <- apply(approx.e,1,make_sss)
 
-trough.x <- ifelse(trough.day > 188, trough.day, trough.day + 365)
-trough.x <- as.matrix(seq(trough.x, by = 365, length.out = 33))
-window <- max(16, round((180*f[1,amp])/2, 0))
+trough.day <-  adjust_trough.day(trough.day$year_min)
+
+trough.x <- as.matrix(seq(trough.x[1], by = 365, length.out = 33))
+
+window <- max(16, round((180*amplitude)/2, 0))
 trough.win.x <- apply(trough.x, 1, FUN=function(x) (x-window):(x+window))
 if(typeof(trough.win.x)=="integer") trough.win.x = rbind(trough.win.x)
+
+
 
 td <- vector(length = 33)
 td.x <- vector(length = 33)
